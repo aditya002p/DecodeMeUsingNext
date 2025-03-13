be88cb7e6bec062960c226be1b180c538142a628
Aditya <ricky.pathak902@gmail.com>
2025-03-13 15:40:59 +0530

    completed the landing page

--- src/components/landing-page/Header.tsx
+++ src/components/landing-page/Header.tsx
@@ -5,8 +5,8 @@ import notes from "../../../public/notes.png";
 const Header = () => {
   return (
     <>
-      <div className="w-full mx-2 py-2 bg-[#50461B] text-white flex items-center justify-center md:text-sm text-xs lg:text-lg font-semibold ">
-        <div className="md:max-w-7xl  w-full flex items-center justify-start gap-2 ">
+      <div className="lg:w-full lg:mx-0 mx-2 p-2 bg-[#50461B] text-white flex items-start justify-start md:text-sm text-xs lg:text-lg font-semibold ">
+        <div className="lg:max-w-7xl flex items-center justify-start gap-2 ">
           <Image src={notes} alt="Notification" width={40} height={40} />
           <span>
             Did You Know? 80% of graduates regret their career choice?
